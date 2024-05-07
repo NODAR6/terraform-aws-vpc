043ad06fad6b812cd318b82721a240a34232737e
NODAR6 <nodarogbaidze1911@gmail.com>
2024-05-06 22:40:35 -0500

    vpc created

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
